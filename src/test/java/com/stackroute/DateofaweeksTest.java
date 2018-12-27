f0d6d001d12945f3b536a4c1b82739529f1710a6
Pratyush Prakash <45649947+pratyushpp@users.noreply.github.com>
2018-12-27 18:55:44 +0530

    Add files via upload

---
+++ src/test/java/com/stackroute/DateofaweeksTest.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class DateofaweeksTest {
+    Dateofaweeks d = new Dateofaweeks();    @Test
+    public void caltest() {
+        assertEquals("First day of week : Mon 24/12/2018 Last day of week : Sun 30/12/2018", d.calender("dd/MM/YYYY"));
+    }    @Test
+    public void caltest1() {
+        assertNotEquals("Last day of week : Mon 24/12/2015 Last day of week : Sun 30/12/2015",d.calender("dd/mm/yyyy"));
+    }
+}
