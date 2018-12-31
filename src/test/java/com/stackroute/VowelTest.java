badd0722afc920ce9ce1cba7e8d2e12d416a338c
kranthichallagulla <kranthichallagulla>
2018-12-31 11:04:22 +0530

    modified vowel and matrix

--- src/test/java/com/stackroute/VowelTest.java
+++ src/test/java/com/stackroute/VowelTest.java
@@ -1,25 +1,20 @@
 package com.stackroute;
-
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
 public class VowelTest
 {
     Vowel vowel=new Vowel();
     @Test
-
     public void removeVowel1()
     {
         String[] result = vowel.removeVowel(new String[] {"India","United States","Germany","Egypt","czechoslovakia"});
         assertArrayEquals(new String[]{"Ind","Untd Stts","Grmny","Egypt","czchslvk"},result);
     }
-
-    @Test
+   @Test
     public void removeVowel2()
     {
         String[] result = vowel.removeVowel(new String[]{"Egypt,czechoslovakia,India,United States,Germany"});
-        assertArrayEquals(new String[]{"Egypt,czchslvk,Ind,Untd Stts,Grmny"},result);
+        assertNotEquals(new String[]{"Egypt,czchslvk,Ind,Untd Stts,Grmny"},result);
     }
     @Test
     public void removeVowelFail()
@@ -27,5 +22,4 @@ public class VowelTest
         String[] result = vowel.removeVowel(new String[]{"India,czechoslovakia"});
         assertNotEquals(new String[]{"India,czechslvk"},result);
     }
-
 }
